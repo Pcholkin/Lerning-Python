4b56e3d2a4e3c4c55fceacd4920964eccd7e2e1b
alexpcholkin <alexpcholkin@gmail.com>
2022-09-06 23:49:18 +0200

    с числами разобрался определение не работает

--- numbers.py
+++ numbers.py
@@ -11,24 +11,33 @@
 # print(id(x))
 # print(type(x))
 
-def print(x):
-    x = 2 + 3
-        print(f"operation plus (+) = " {x})
+# x=2+3
+# print(f'operation plus (+) ={x}')       # + сложение цифр
+#
+# y = 10 - 3
+# print(f'operation minus (-) = {y}')     # - вычитание
+#
+# z = 5 * 5
+# print(f'operation multiplication (*) = {z}')    # * - умножение
+#
+# e = 12 / 7
+# print(f'operation division (/) = {e}')  # / - деление. Узнать как ограничить количество цифр после точки
+#
+# r = 12 // 7
+# print(f'operation integer division (//) = {r}')  # сколько целых чисел помещаеться в делимом, от есть 12//7=1
+#
+# q = 10 % 6
+# print(f'operation remainder of the division (%) = {q}')  # вотзврат остатка от деление (не понимаю)
+#
+# t = 3 ** 3
+# print(f'operation exponentiation (**) = {t}')       # ** - возведение в степень
 
-    y = 10 - 3
-        print(f'operation minus (-) = {y}')
+def print_hi(n2um):
+    print(f'Введи число {n2um}')
 
-    z = 5 * 5
-        print(f'operation multiplication (*) = {z}')
+#    n2um=input({n2um})
 
-    e = 12 / 7
-        print(f'operation division (/) = {e}')  # узнать как ограничить количество цифр после точки
-
-    r = 12 // 7
-        print(f'operation integer division (//) = {r}')  # сколько целых чисел помещаеться в делимом, от есть 12//7=1
-
-    q = 1 % 4
-        print(f'operation remainder of the division (%) = {q}')  # вотзврат остатка от деление
-
-    #x = 10 % 4  # % - это остаток от деления
-    #print(x)
+    if n2um % 2 == 0:
+        print(f'Число {n2um} четное')
+    else:
+        print(f'Число {n2um} не четное')
