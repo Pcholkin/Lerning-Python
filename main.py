eaecb9e906138bbe330006c9c4f4aedccb96195d
alexpcholkin <alexpcholkin@gmail.com>
2022-09-06 13:58:29 +0200

    разбирался с операциями с числами

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-# def - define обявления функции (какие бівают функции??)
 
-def print_hi(name):
+
+def print_hi(name):                     # def - define обявления функции (какие бывают функции??)
     print(f'Hi, {name}')
 
 if __name__ == '__main__':
-    name=input("What is your name?")
+    name=input("What is your name?")    # input - функция чтобы считать данные от пользователя (ввести имя число итд.)
     print(f'Fuck you {name}!')
---
+++ numbers.py
@@ -0,0 +1,34 @@
+# x=10
+# print(type(x))
+
+# y=12.4
+# print(type(y))
+
+# x=5
+# print(id(x))
+# print(type(x))
+# x=2.2
+# print(id(x))
+# print(type(x))
+
+def print(x):
+    x = 2 + 3
+        print(f"operation plus (+) = " {x})
+
+    y = 10 - 3
+        print(f'operation minus (-) = {y}')
+
+    z = 5 * 5
+        print(f'operation multiplication (*) = {z}')
+
+    e = 12 / 7
+        print(f'operation division (/) = {e}')  # узнать как ограничить количество цифр после точки
+
+    r = 12 // 7
+        print(f'operation integer division (//) = {r}')  # сколько целых чисел помещаеться в делимом, от есть 12//7=1
+
+    q = 1 % 4
+        print(f'operation remainder of the division (%) = {q}')  # вотзврат остатка от деление
+
+    #x = 10 % 4  # % - это остаток от деления
+    #print(x)
