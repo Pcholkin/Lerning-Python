531b072252e604494c1970a6e645dcb134ee9437
Pcholkin <alexjpcholkin@gmail.com>
2022-09-07 23:13:46 +0300

    Считаем расход топлива на нужное расстояние

---
+++ Costs of travel.py
@@ -0,0 +1,29 @@
+def Costs(LPG):
+
+    print(f'How is price of LPG? {LPG}')
+
+def Costs2(GAS):
+
+    print(f'How is price of GAS? {GAS}')
+
+if __name__ == '__main__':
+
+    LPG_100=16      #расход машины на 100 км газа
+    GAS_100=14      #расход машины на 100 км бенза
+
+    LPG = float(input("How is price of LPG?"))
+    GAS = float(input("How is price of GAS?"))
+
+    LPGS = round(LPG_100*LPG, 1)      #умножаем чтобы получить стоимость 100 km
+    GASS = round(GAS_100*GAS, 1)
+
+    print(f'100 km by LPG cost {LPGS} Euro')    #показываем стоимость 100 км
+    print(f'100 km by GAS cost {GASS} Euro')
+
+    x=float(input('How many kilometers do you want to drive?'))     #узнаем сколько надо проехать км
+
+    HOW_KM_LPG = float(round(x*LPGS/100, 1))    #делим по формуле чтобы узнать расход
+    HOW_KM_GAS = float(round(x*GASS/100, 1))
+
+    print(f'Price of {x} KM by LPG = {HOW_KM_LPG} Euro')    #показываем стоимость нужного расстояния
+    print(f'Price of {x} KM by GAS = {HOW_KM_GAS} Euro')
