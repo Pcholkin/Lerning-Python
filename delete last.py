4874bd451747d038b265363405685aae69d80af3
Alex Pcholkin <alexjpcholkin@gmail.com>
2022-09-15 13:49:27 +0200

    delete last char 2 ver. and show deleted

--- delete last.py
+++ delete last.py
@@ -1,12 +1,12 @@
 ss = input("Enter:")
 l = len(ss)
-rem_lst = ss[:l-1]
+rem_lst = ss[:l-1]   #удаляем последний символ
 print(rem_lst)
-show_last = ss[-1]
+show_last = ss[-1]  #показываем последний символ
 print(show_last)
 
 #еще так можно
 
 mm = input('Enter:')
-Rem_lst2 = mm[:-1]
+Rem_lst2 = mm[:-1]  #тоже удаляем последний символ
 print(Rem_lst2)
