a2f01cb39b648f978fa9f5ad5c636855c9b21a4b
Pcholkin <alexjpcholkin@gmail.com>
2022-09-23 01:11:21 +0300

    Бот собирает инфу

--- HomeWork 3/Demo Bot.py
+++ HomeWork 3/Demo Bot.py
@@ -6,17 +6,94 @@ and I need your sizes.
 ''')
 print('Ok, what\'s your name?')
 student = input()
-print(f'''
-Cool, {student}, what is your gender?
-male / female / animal''')
-gender = input()
 
-if 'male' in gender:
-    print('OK handsome, now how you\'r tall?')
-    if 'female' in gender:
-        print('OK beauty, now how you\'r tall?')
-    elif 'animal' in gender:
+while True:
+
+    print(f'''
+Cool, {student}, what is your gender?
+boy / girl / animal''') # я сначало написал male и female и потом час ломал голову почему при feMALE отрабатывает MALE.
+    gender = str.capitalize(input())
+
+    if 'Boy' in gender:
+        print('OK handsome, how you\'r tall?')
+        tall_b = int(input())
+        if tall_b > 180:
+                print('Good, I hope you in basketball team!')
+        else:
+            print('I got it, next question is: ')
+        break
+    elif 'Girl' in gender:
+            print('OK beauty, how you\'r tall?')
+            tall_b = int(input())
+            if tall_b > 170:
+                    tall_g_num = int(input('I like it, give me your phone number :) : '))  # ну а шо (:
+            else:
+                    print('I got it, next question is:')
+            break
+
+    elif 'Animal' in gender:
         print('Go back to the zoo, you dirty animal!')
+        exit()
+
+    else:
+        print(f'Are you sure, that {gender} is right value? let\'s  try again:')
+
+while True:
+    print('''
+What colour gamma you prefer?
+We have 3 options:
+    1. Gray
+    2. Green
+    3. Cherry
+    4. Black
+''')
+
+    color = str.capitalize(input())
+
+    if 'Gray' in color or '1' in color:
+        print('Gray it\'s a good choice!')
+        break
+    elif '2' in color or 'Green' in color:
+        print('Green it\'s a good choice!')
+        break
+    elif '3' in color or 'Cherry' in color:
+        print('Good choice!')
+        break
+    elif '4' in color or 'Black' in color:
+        print('Total Black, cool!')
+        break
+    else:
+        print('Not time for games, choice one from list!')
+
+
+while True:
+    print('''
+Okay, last question for today, about style:
+    
+     Classic
+     Fashion
+     Modern
+     Wild
+''')
+    style = str.capitalize(input('Make a right choice: '))
+
+    if 'Classic' in style:
+        print('Of course classic!')
+        break
+    else:
+        print('think again (:')
+
+print(f'''
+Okay, I\'ll send this information in s school office:
+ 
+Gender:    {gender}
+Tall:      {tall_b}
+Student:   {student}
+Colour:    {color}
+Style:     {style}  
+''')
+if tall_g_num > 0:
+    print('Your phone number I\'ll send to developers office (: ')
 else:
-    print('hello beauty!')
-    print('')
+    pass
+
